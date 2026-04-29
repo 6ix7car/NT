e7b56053d5e540bacf15f96b3b63bf7877be1c36
6ix7car <mihanavi04@mail.ru>
2026-04-29 22:25:43 +1000

    feat: добавил решение StatsAgent - классы program, DbHelper, Stats

---
+++ StatsAgent/Program.cs
@@ -0,0 +1,29 @@
+﻿using NotesApp;
+using System;
+using System.Threading;
+
+namespace StatsAgent
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "Stats Agent";
+            Console.WriteLine("Агент мониторинга запущен. Сбор статистики каждые 60 секунд.");
+
+            while (true)
+            {
+                try
+                {
+                    Stats.SaveLocalStatsToDb();
+                    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Статистика сохранена в БД.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Ошибка: {ex.Message}");
+                }
+                Thread.Sleep(60000);
+            }
+        }
+    }
+}
